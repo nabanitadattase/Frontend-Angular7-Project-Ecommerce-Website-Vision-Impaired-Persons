eaddccd22da5036133d06f9202ecac90c7493770
nabanitadattase <47370928+nabanitadattase@users.noreply.github.com>
2019-10-01 11:10:52 +0200

    E-commerce Website For Vision Impaired Persons

--- supernova/src/environments/environment.prod.ts
+++ supernova/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCLluE3wVYyYvDjFqN0edjjDICQxNfrTcw',
+    authDomain: 'supernova-46a07.firebaseapp.com',
+    databaseURL: 'https://supernova-46a07.firebaseio.com',
+    projectId: 'supernova-46a07',
+    storageBucket: '',
+    messagingSenderId: '1069487814937',
+    appId: '1:1069487814937:web:19e88a67ea03d0d708ed88',
+    measurementId: 'G-46MHSSP4N0',
+  },
 };
